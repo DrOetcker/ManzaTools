35154375ec7ce604cf24ac50721eb73499e4d34c
DrOetcker <collector5000@web.de>
2023-12-07 14:25:20 +0100

    feat: Init develop commit with build and no publish from develop

---
+++ ManzaTools/ManzaTools.cs
@@ -0,0 +1,31 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+
+namespace ManzaTools
+{
+    public class ManzaTools : BasePlugin, IPluginConfig<ManzaToolsConfig>
+    {
+        public override string ModuleName => "plugin name";
+        public override string ModuleAuthor => "author";
+        public override string ModuleVersion => "0.0.1";
+
+
+        public ManzaToolsConfig Config { get; set; } = new();
+
+        public void Load(bool hotReload)
+        {
+
+        }
+
+        public void Unload(bool hotReload)
+        {
+
+        }
+
+        public void OnConfigParsed(ManzaToolsConfig config)
+        {
+            this.Config = config;
+        }
+    }
+
+}
---
+++ ManzaTools/ManzaToolsConfig.cs
@@ -0,0 +1,14 @@
+﻿using CounterStrikeSharp.API.Core;
+using System.Text.Json.Serialization;
+
+namespace ManzaTools
+{
+    public class ManzaToolsConfig : BasePluginConfig
+    {
+        [JsonPropertyName("ConfigName")]
+        public string ConfigName { get; set; } = "value";
+
+        [JsonPropertyName("ConfigNameSecond")]
+        public string ConfigNameSecond { get; set; } = "value";
+    }
+}
