a7fa11c9096c3a334924df1320999b9a67961450
DrOetcker <collector5000@web.de>
2023-12-07 16:32:00 +0100

    feat: Load Practice as default

--- ManzaTools/ManzaTools.cs
+++ ManzaTools/ManzaTools.cs
@@ -1,6 +1,7 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using ManzaTools.Config;
+using ManzaTools.Models;
 using ManzaTools.Services;
 using ManzaTools.Utils;
 
@@ -12,16 +13,21 @@ namespace ManzaTools
         public override string ModuleAuthor => "DrOetcker";
         public override string ModuleVersion => "0.0.1";
         public ManzaToolsConfig Config { get; set; } = new();
-        private CfgShipper _cfgShipper;
 
-        public ManzaTools(CfgShipper cfgShipper)
+        private readonly CfgShipper _cfgShipper;
+        private readonly GameModeService _gameModeService;
+
+        public ManzaTools(CfgShipper cfgShipper, GameModeService gameModeService)
         {
             _cfgShipper = cfgShipper;
+            _gameModeService = gameModeService;
         }
 
         public override void Load(bool hotReload)
         {
             _cfgShipper.InitDefaultCfgs(ModuleDirectory);
+            if(!hotReload)
+                _gameModeService.LoadGameMode(GameModeEnum.Practice);
         }
 
         public override void Unload(bool hotReload)
--- ManzaTools/Utils/Responses.cs
+++ ManzaTools/Utils/Responses.cs
@@ -7,18 +7,18 @@ namespace ManzaTools.Utils
     {
         public static void ReplyToPlayer(string message, CCSPlayerController player, bool isError = false, bool toConsole = false)
         {
-                if (toConsole)
-                    player.PrintToConsole($"{(isError ? Statics.ConsolePrefixError : Statics.ConsolePrefix)} {message}");
-                else
-                    player.PrintToChat(Statics.GetChatText(message, isError));
+            if (toConsole)
+                player.PrintToConsole($"{(isError ? Statics.ConsolePrefixError : Statics.ConsolePrefix)} {message}");
+            else
+                player.PrintToChat(Statics.GetChatText(message, isError));
         }
 
         public static void ReplyToServer(string message, bool isError = false, bool toConsole = false)
         {
-                if (toConsole)
-                    Server.PrintToConsole($"{(isError ? Statics.ConsolePrefixError : Statics.ConsolePrefix)} {message}");
-                else
-                    Server.PrintToChatAll(Statics.GetChatText(message, isError));
+            if (toConsole)
+                Server.PrintToConsole($"{(isError ? Statics.ConsolePrefixError : Statics.ConsolePrefix)} {message}");
+            else
+                Server.PrintToChatAll(Statics.GetChatText(message, isError));
         }
 
     }
